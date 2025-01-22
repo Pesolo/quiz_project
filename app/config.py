9929ec3257a0c3f6cd36206b7fe1cf6b233a8851
Pesolo <ahotinpesu@gmail.com>
2025-01-22 13:11:44 +0100

    db

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://grp_7:VtjQjmssE4kcHxeeC1QOc5iRYuC5iw1F@dpg-cu7cgs0gph6c73ba1hs0-a/software_proj', '').replace(
         'postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
