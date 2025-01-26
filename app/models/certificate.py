1c6a5693ab7e1dd1d4f93c81af2bab81e53bee7e
Pesolo <ahotinpesu@gmail.com>
2025-01-26 23:50:19 +0100

    sql

--- app/models/certificate.py
+++ app/models/certificate.py
@@ -4,6 +4,6 @@ from datetime import datetime, timezone
 class Certificate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    category = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50), nullable=True)
     level = db.Column(db.Integer, nullable=False)
     date_earned = db.Column(db.DateTime, default=datetime.now(timezone.utc))
--- app/models/quiz.py
+++ app/models/quiz.py
@@ -6,5 +6,5 @@ class QuizAttempt(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category = db.Column(db.String(50), nullable=False)
     score = db.Column(db.Integer, nullable=False)
-    difficulty = db.Column(db.String(20), nullable=False)
+    difficulty = db.Column(db.String(20), nullable=True)
     date_attempted = db.Column(db.DateTime, default=datetime.now(timezone.utc))
