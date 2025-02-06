923a888df8d2e0bf10899ad0528e7c794d7fd309
Pesolo <ahotinpesu@gmail.com>
2025-02-06 22:16:13 +0100

    admon init

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,7 @@ from .auth import auth_bp
 from .quiz import quiz_bp
 from .certificate import cert_bp
 from .profile import profile_bp
+from .admin import admin_bp
 
 # Register all blueprints
 def init_routes(app):
@@ -10,3 +11,4 @@ def init_routes(app):
     app.register_blueprint(quiz_bp)
     app.register_blueprint(cert_bp)
     app.register_blueprint(profile_bp)
+    app.register_blueprint(admin_bp)
